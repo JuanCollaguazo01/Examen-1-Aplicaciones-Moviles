aa5bfb8111959eb188b737929c66fee5149612f2
Juan Collaguazo <juan.collaguazo01@epn.edu.ec>
2021-07-28 17:41:05 -0500

    Blank

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDZatFVi5JFHOSmXk53fBbJEppLrctfQEs",
+    authDomain: "crud2-41c4b.firebaseapp.com",
+    databaseURL: "https://crud2-41c4b-default-rtdb.firebaseio.com",
+    projectId: "crud2-41c4b",
+    storageBucket: "crud2-41c4b.appspot.com",
+    messagingSenderId: "55290192281",
+    appId: "1:55290192281:web:a310a21cc36a6830b37308",
+    measurementId: "G-XGJ4N5KH8X"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDZatFVi5JFHOSmXk53fBbJEppLrctfQEs",
+    authDomain: "crud2-41c4b.firebaseapp.com",
+    databaseURL: "https://crud2-41c4b-default-rtdb.firebaseio.com",
+    projectId: "crud2-41c4b",
+    storageBucket: "crud2-41c4b.appspot.com",
+    messagingSenderId: "55290192281",
+    appId: "1:55290192281:web:a310a21cc36a6830b37308",
+    measurementId: "G-XGJ4N5KH8X"
+  }
 };
 
 /*
